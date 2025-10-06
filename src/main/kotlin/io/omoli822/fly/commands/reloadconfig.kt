1391e0d903507f84b023afc604d797561c311ed3
omoli822 <omoliom@hotmail.com>
2025-10-06 12:02:59 +0800

    fly 3.0.2 fix

--- src/main/kotlin/io/omoli822/fly/commands/reloadconfig.kt
+++ src/main/kotlin/io/omoli822/fly/commands/reloadconfig.kt
@@ -1,29 +1,28 @@
 package io.omoli822.fly.commands
+
 import org.bukkit.command.Command
 import org.bukkit.command.CommandExecutor
 import org.bukkit.command.CommandSender
-import org.bukkit.entity.Player
 import org.bukkit.plugin.java.JavaPlugin
 
 class ReloadConfig(private val plugin: JavaPlugin) : CommandExecutor {
 
-    override fun onCommand(sender: CommandSender, command: Command, label: String, args: Array<String>): Boolean {
+    override fun onCommand(
+        sender: CommandSender,
+        command: Command,
+        label: String,
+        args: Array<String>
+    ): Boolean {
+        // Permission check
         if (!sender.hasPermission("fly.reload")) {
             sender.sendMessage("§cYou do not have permission to run this command!")
             return true
         }
 
-        if (args.isEmpty()) {
-            sender.sendMessage("§eUsage: /flyreloadconfig")
-            return true
-        }
-
-        // Combine all args into one message
-        val newMessage = args.joinToString(" ")
+        // Reload the plugin config
         plugin.reloadConfig()
+        sender.sendMessage("§aFly plugin config reloaded successfully!")
 
-
-        sender.sendMessage("§aRELOADED CONFIG OF FLY!")
         return true
     }
 }
